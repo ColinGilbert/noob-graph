2afabd657996f3d891e80dd22608320f88d5603b
Colin <colingilbert86@gmail.com>
2016-07-12 14:29:34 -0600

    Remove rdestl

--- test/main.cpp
+++ test/main.cpp
@@ -2,54 +2,82 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
-
-static const std::string n_of("number of");
-static const std::string nodes_str("node");
-static const std::string edges_str("edge");
-
-static const std::string h_str("handle");
-static const std::string expect_str("expected");
-static const std::string got_str("got");
-
-static unsigned int test_number = 0;
+#include <locale>
 
 std::string space(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << " ";
+	return ss.str();
 }
 
 std::string comma(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << ", ";
+	return ss.str();
 }
 
 std::string open_p(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << "(";
+	return ss.str();
 }
 
 std::string close_p(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << ")"
+	return ss.str();
 }
 
 std::string cap(const std::string& s)
 {
+	std::locale loc;
+	std::string temp(s);
+	std::toupper(temp[0], loc);
+	return temp;
+}
+
+std::string allcaps(const std::string& s)
+{
+	std::locale loc;
+	std::string temp(s);
+	for (size i = 0; i < s.length(); ++i)
+	{
+		std::toupper(temp[i], loc);
+	}
+	return temp;
 }
 
 std::string period(const std::string& s)
 {
+	std::stringstream ss;
+	ss << ".";
+	return ss.str();
 }
 
 std::string colon(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << ": ";
+	return ss.str();
 }
 
 std::string semicolon(const std::string& s)
 {
+	std::stringstream ss;
+	ss << s << "; ";
+	return ss.str();
 }
 
 void log(const std::string& s)
 {
 	std::cout << s << std::endl;
 }
+
+static unsigned int test_number = 0;
 
 void log_test(bool b, const std::string& test_name)
 {
@@ -66,6 +94,15 @@ void log_test(bool b, const std::string& test_name)
 	log(ss.str());
 	++test_number;
 }
+
+
+static const std::string n_of_str("number of");
+static const std::string nodes_str("node");
+static const std::string edges_str("edge");
+static const std::string handle_str("handle");
+static const std::string expect_str("expected");
+static const std::string got_str("got");
+
 
 void test_node_add(noob::node_h expected_handle, uint32_t expected_num_nodes)
 {
