d0247fcaf40168141bfb8bad1f2c4da950df1c6e
Colin <colingilbert86@gmail.com>
2016-08-31 21:17:20 -0600

    Tweak

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -1,3 +1,4 @@
+// Near minimal as graphs get.
 #pragma once
 
 #include <cstdint>
@@ -5,24 +6,27 @@
 #include <assert.h>
 
 #include <rdestl/vector.h>
-#include <rdestl/slist.h>
+#include <rdestl/list.h>
+
 
 
 namespace noob
 {
+
 	struct graph
 	{
-
 		struct node
 		{
 			node() noexcept(true) : valid(true) {}
+
+			static constexpr uint32_t invalid = std::numeric_limits<uint32_t>::max();
+
 			bool valid;
 			rde::vector<uint32_t> outgoing;
-
 		};
 
 		// Member functions
-		bool node_exists(uint32_t n) const
+		bool node_valid(uint32_t n) const
 		{
 			if (n > nodes.size() - 1)
 			{
